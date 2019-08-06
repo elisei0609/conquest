7784eb9ebb905b08d6581e8a78d16f843a6b67e0
Yelysei <elisei2015@gmail.com>
2019-08-06 16:26:43 +0300

    hover effects for buttons and svg items

--- js/script.js
+++ js/script.js
@@ -15,13 +15,13 @@ function loadGoods() {
            out+= '<p class="catalog-items__price">'+ data[key]['cost'] + '</p>';
            out+= '</div>';
            out+= '<div class="catalog-items-social">';
-           out+= '<button href="#">';
+           out+= '<button class="catalog-social__svg like">';
            out+= '<svg width="21" height="18" viewBox="0 0 21 18">';
            out+= '<use xlink:href="#love-svg">';
            out+= '</use>';
            out+= '</svg>';
            out+= '</button>';
-           out+= '<button href="#">';
+           out+= '<button class="catalog-social__svg elem">'
            out+= '<svg width="18" height="16" viewBox="0 0 18 16">';
            out+= '<use xlink:href="#box-svg">';
            out+= '</use>';
